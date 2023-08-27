1c4f517bb5462381319f94404c10c2272e22b62e
amir4774 <amirabbassi1382@gmail.com>
2023-08-28 01:15:20 +0330

    add revalidate to api/search

--- app/api/search/route.tsx
+++ app/api/search/route.tsx
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const name = searchParams.get("query") as string;
-  const res = await fetch(`https://api.github.com/users/${name}`);
+  const res = await fetch(`https://api.github.com/users/${name}`, {
+    next: {
+      revalidate: 100,
+    },
+  });
   const data = await res.json();
 
   return NextResponse.json(data);
