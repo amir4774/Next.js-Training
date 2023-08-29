a8517184243a57c57af9fc4048911b00c298d784
amir4774 <amirabbassi1382@gmail.com>
2023-08-29 15:44:51 +0330

    show active links in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,45 @@
 "use client";
 import Link from "next/link";
-import stylesHeader from "../styles/header.module.css";
+import "../styles/header.css";
 import { useSelector } from "react-redux";
 import { RootState } from "../Redux/Store";
 
 const Header = () => {
   const userContent = useSelector((state: RootState) => state.user.user);
 
+  const activeLink = (selectedLink: HTMLAnchorElement) => {
+    // Remove choose class from all links
+    const links = document.querySelectorAll(".link");
+    links.forEach((link) => link.classList.remove("choose"));
+    // Add choose class to the selected link
+    selectedLink.classList.add("choose");
+  };
+
   return (
-    <header className={stylesHeader.header}>
-      <h1 className={stylesHeader.title}>GITHUB PROFILE FINDER</h1>
-      <nav className={stylesHeader.links}>
-        <Link href="/">Home</Link>
-        <Link href="/search">Search</Link>
-        <Link href="/person">
+    <header className="header">
+      <h1 className="title">GITHUB PROFILE FINDER</h1>
+      <nav className="links">
+        <Link
+          className="link choose"
+          href="/"
+          onClick={(e) => activeLink(e.target as HTMLAnchorElement)}
+        >
+          Home
+        </Link>
+
+        <Link
+          className="link"
+          href="/search"
+          onClick={(e) => activeLink(e.target as HTMLAnchorElement)}
+        >
+          Search
+        </Link>
+
+        <Link
+          className="link"
+          href="/person"
+          onClick={(e) => activeLink(e.target as HTMLAnchorElement)}
+        >
           {userContent.login ? userContent.login : "Person"}
         </Link>
       </nav>
